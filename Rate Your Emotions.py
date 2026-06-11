c0b77c11601489be5d53bbeeeea54e4093dbf670
Bobby Smith <bobbysmith@Bobbys-MacBook-Air-10.local>
2026-06-11 13:43:00 -0400

    continued conversation

--- Rate Your Emotions.py
+++ Rate Your Emotions.py
@@ -1,3 +1,5 @@
-Answer = input("How are you feeling today?")
-if Answer == "bad":
-    print("sorry to hear that,")
+Answer = input("Rate your day so far (1-10): ")
+if Answer <= "4":
+    input("sorry to hear that, wanna talk about it? ")
+if Answer >= "5":
+    input("great!, let's talk about it :) ")
